c14a243d55203571a49549434dceaf3d367a2dda
Tateshiki0529 <lab@ttsk3.net>
2020-12-27 20:43:20 +0900

    Changed output

--- refreshCache.php
+++ refreshCache.php
@@ -1,12 +1,12 @@
 <?php
 
 	// Cache refresh program <refreshCache.php>
+	error_reporting(!E_ALL);
 
 	// Load cache control program
 	require_once dirname(__FILE__)."/libs/class.cache.php";
 
 	$cache = new Cache();
 	$time = (int)date("G") % 6;
-	echo $cache->updateCache($time);
-
+	var_dump($cache->updateCache($time));
 ?>
