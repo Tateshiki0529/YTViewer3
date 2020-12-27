2bb1871717fb20b3554ee43b972f2cb87a6509b1
Tateshiki0529 <lab@ttsk3.net>
2020-12-27 14:42:17 +0900

    Edit test file

--- libs/tests.php
+++ libs/tests.php
@@ -11,7 +11,7 @@
 
 	header("Content-Type: application/json");
 
-	/*switch ($_GET["m"]) {
+	switch ($_GET["m"]) {
 		case 'video':
 			var_dump($api->getVideo($_GET["id"]));
 			break;
@@ -27,8 +27,8 @@
 		default:
 			# code...
 			break;
-	}*/
+	}
 
-	# var_dump($cache->updateCache($_GET["circle"]));
+	#echo json_encode($cache->updateCache($_GET["circle"]), JSON_PRETTY_PRINT);
 
 ?>
