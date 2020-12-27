5ce324ed2a55c9c644698650204ab60bb4c7c09c
Tateshiki0529 <lab@ttsk3.net>
2020-12-27 23:38:59 +0900

    Created site constant definition file

---
+++ libs/const.site.php
@@ -0,0 +1,11 @@
+<?php
+
+	// Site constant definition file <const.site.php>
+
+	define('SITE_DOMAIN', 'https://ytv3.ml/');
+
+	define('SITE_URL_HOME', SITE_DOMAIN.'index.php');
+
+	define('PAGE_HOME', 1);
+
+?>
