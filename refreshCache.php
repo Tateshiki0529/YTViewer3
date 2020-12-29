f0ac9babd17de86fa375871a060c97cbf99b63e5
Tateshiki0529 <lab@ttsk3.net>
2020-12-29 20:59:38 +0900

    Added statistics process

--- refreshCache.php
+++ refreshCache.php
@@ -2,11 +2,18 @@
 
 	// Cache refresh program <refreshCache.php>
 	error_reporting(!E_ALL);
+	header("Content-Type: application/json");
 
 	// Load cache control program
 	require_once dirname(__FILE__)."/libs/class.cache.php";
 
+	require_once dirname(__FILE__)."/libs/class.sitestat.php";
+
 	$cache = new Cache();
+	$stat = new SiteStatistics();
 	$time = (int)date("G") % 6;
 	var_dump($cache->updateCache($time));
+	if (date("i") == "00" or date("i") == "30") foreach ([CACHEMODE_VIDEO, CACHEMODE_CHANNEL, CACHEMODE_PLAYLIST, CACHEMODE_PLAYLISTCONTENTS] as $v) {
+		var_dump($stat->saveStats($v));
+	}
 ?>
